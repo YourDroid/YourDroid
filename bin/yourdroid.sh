701e86b1de46b427cbedbd9257c4f7faa7f1799a
Profi_GMan <Profi_GMan@mail.ru>
2018-05-02 12:38:16 +0300

    upd

---
+++ bin/yourdroid.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+dirname=`dirname $0`
+tmp="${dirname#?}"
+
+if [ "${dirname%$tmp}" != "/" ]; then
+dirname=$PWD/$dirname
+fi
+LD_LIBRARY_PATH=$dirname
+export LD_LIBRARY_PATH
+"$dirname/yourdroid" "$@"
